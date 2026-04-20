4f4f507adce8e079c5358bb7d2ba2cbe5c4db2ca
Taufik Napitupulu <206003720+aaravitha@users.noreply.github.com>
2026-04-20 09:41:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 
+// IMPORTANT: change this to your exact repo name for GitHub Pages
 export default defineConfig({
   base: "/minecraft-3d-skin-preview-qmila/"
 });
